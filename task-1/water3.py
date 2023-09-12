5a539c923f2161c8bf5bbb45486c3d64aa8f53d2
Thanabordin-Akkharachinoret <144810754+Thanabordin-Akkharachinoret@users.noreply.github.com>
2023-09-12 22:31:16 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -6,4 +6,11 @@ def is_goal(s):
 
 def successors(s):
     a, b, c = s
-    return []
+    new_a, new_b = a-5, b+5
+    new_b, new_c = b-3, c+3
+    new_a, new_c = a+3, c-3
+    new_b, new_c = b-2, c+2
+    new_a, new_b = a-5, b+5
+    new_b, new_c = b-1, c+1
+    new_a, new_c = a+3, c-3
+    return (4,4,0)
